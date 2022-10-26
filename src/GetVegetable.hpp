bea654c081dc2d3957e90159fb03bd305e0be8d0
DrEden33773 <edwardwang33773@gmail.com>
2022-10-26 13:03:17 +0800

    update: AC

--- src/GetVegetable.hpp
+++ src/GetVegetable.hpp
@@ -11,6 +11,9 @@
 
 #pragma once
 
+// This is a variation of traditional `in out vehicle`
+// It judges the time of `both on the vehicle`
+
 #include <algorithm>
 #include <iostream>
 #include <utility>
@@ -19,6 +22,11 @@
 class GetVegetable {
     std::vector<std::pair<int, int>> ATime;
     std::vector<std::pair<int, int>> BTime;
+
+    /// @brief @b 0__|=>__used_by_non
+    /// @brief @b 1__|=>__used_by_one
+    /// @brief @b 2__|=>__used_by_two
+    std::vector<int> TimeTable;
 
     int END_OF_PERIOD = 0;
     int BEG_OF_PERIOD = 0;
@@ -32,8 +40,9 @@ public:
         std::cin >> num;
         number = num;
 
-        ATime = std::vector<std::pair<int, int>>(num + 3);
-        BTime = std::vector<std::pair<int, int>>(num + 3);
+        ATime     = std::vector<std::pair<int, int>>(num + 3);
+        BTime     = std::vector<std::pair<int, int>>(num + 3);
+        TimeTable = std::vector<int>(1000003, 0);
 
         for (int i = 1; i <= num; ++i) {
             int ABeg = 0;
@@ -42,6 +51,11 @@ public:
             BEG_OF_PERIOD = std::min(BEG_OF_PERIOD, ABeg);
             END_OF_PERIOD = std::max(END_OF_PERIOD, AEnd);
             ATime[i]      = std::make_pair(ABeg, AEnd);
+
+            for (int j = ABeg; j < AEnd; ++j) {
+                ++TimeTable[j];
+            }
+            --TimeTable[AEnd];
         }
 
         for (int i = 1; i <= num; ++i) {
@@ -51,10 +65,19 @@ public:
             BEG_OF_PERIOD = std::min(BEG_OF_PERIOD, BBeg);
             END_OF_PERIOD = std::max(END_OF_PERIOD, BEnd);
             BTime[i]      = std::make_pair(BBeg, BEnd);
+
+            for (int j = BBeg; j < BEnd; ++j) {
+                ++TimeTable[j];
+            }
+            --TimeTable[BEnd];
         }
     }
     void process() {
-        // pre opt (might be wrong)
+        for (int i = BEG_OF_PERIOD; i <= END_OF_PERIOD; ++i) {
+            if (TimeTable[i] == 2) {
+                ++answer;
+            }
+        }
     }
     void output() {
         std::cout << answer;
