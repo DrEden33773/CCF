a77439db2ad572d673cd89e6df14e15881b9122a
DrEden33773 <edwardwang33773@gmail.com>
2022-11-24 15:22:46 +0800

    new proj: TreasureHunter

---
+++ src/TreasureHunter.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file TreasureHunter.hpp
+ * @author Eden (edwardwang33773@gmail.com)
+ * @brief @b 202206-2
+ * @version 0.1
+ * @date 2022-11-24
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+
+class TreasureHunter {
+};
